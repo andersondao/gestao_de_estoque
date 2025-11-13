8f90c8834518777a6e171e0c34a9701ded26886a
Anderson Andrade <andersondao@gmail.com>
2025-11-13 14:53:20 -0300

    Implementando funções

--- main.py
+++ main.py
@@ -1,17 +1,60 @@
-nome_produto = str(input("Digite o nome do produto: "))
-sku = str(input("Digite o SKU do produto: "))
-quantidade_estoque = int(input("Digite a quantidade do produto em estoque: "))
-preco_unitario = float(input("Digite o preço unitário do produto: "))
+#Sistema de Gestão de Estoque
 
-print("\n ---Produto adicionado---")
-print(f"Nome: {nome_produto}")
-print(f"SKU: {sku}")
-print(f"Quantidade: {quantidade_estoque}")
-print(f"Preço R$: {preco_unitario:.2f}")
+def adicionar_produto(nome, sku, qtd, preco):
+    
+    estoque.append({"nome": nome, "sku": sku, "qtd": qtd, "preco": preco})
+    print("\n ---Produto adicionado---")
+    print(f"Nome: {nome}")
+    print(f"SKU: {sku}")
+    print(f"Quantidade: {qtd}")
+    print(f"Preço R$: {preco:.2f}")
 
-if quantidade_estoque <= 10:
-    print("Estoque baixo")
-elif quantidade_estoque <= 20:
-    print("Estoque crítico")
-else:
-    print("Adequado")
+def listar_produtos():
+    if not estoque:
+        print("Estoque vazio.")
+        return
+    print("\n---Produtos em Estoque---")
+    for produto in estoque:
+        print(f"SKU:{produto['sku']}, Nome:{produto['nome']}, Quantidade:{produto['qtd']}, Preço: R${produto['preco']:.2f}")
+
+def atualizar_quantidade(estoque, sku_procurado, nova_qtd):
+    encontrado = False
+    for produto in estoque:
+        if produto['sku'] == sku_procurado:
+            produto['qtd'] = nova_qtd
+            encontrado = True
+            print(f"Quantidade do produto {sku_procurado} atualizada para {nova_qtd}.")
+            break
+    if not encontrado:
+        print(f"Produto com SKU '{sku_procurado}' não encontrado")
+
+estoque = []
+
+while True:
+    print("\n--- Sistema de Gestão de Estoque ---")
+    print("1. Adicionar Produto")
+    print("2. Listar Produtos")
+    print("3. Atualizar Quantidade")
+    print("4. Sair")
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+        sku = str(input("Digite o SKU do produto: "))
+        nome = str(input("Digite o nome do produto: "))
+        qtd = int(input("Digite a quantidade do produto em estoque: "))
+        preco = float(input("Digite o preço unitário do produto: "))
+        adicionar_produto(sku, nome, qtd, preco)
+        continue
+    elif opcao == '2':
+        listar_produtos()
+        continue
+    elif opcao == '3':
+        sku_procurado = input("Digite o SKU procurado: ")
+        nova_qtd = input("Digite a nova quantidade: ")
+        atualizar_quantidade(estoque, sku_procurado, nova_qtd)
+        continue
+    elif opcao == '4':
+        print("Saindo do sistema. Até mais!")
+        break 
+    else:
+        print("Opção inválida. Tente novamente.") 
